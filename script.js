57373eb3467029170af1fe289e98be7ad92511fb
AlvaroPersichini <alvaro.persichini@gmail.com>
2025-10-28 11:49:05 -0300

    mejoraBotonHamb

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function toggleMenu() {
   const menuOverlay = document.getElementById('menuOverlay');
+  const btn = document.getElementById('hamburger');
   const body = document.body;
-  
+
+
   menuOverlay.classList.toggle('active');
+  btn.classList.toggle('active');
   body.classList.toggle('no-scroll');
-  }
+}
 
 // Cerrar menú al hacer clic fuera del contenido
-document.getElementById('menuOverlay').addEventListener('click', function(e) {
+document.getElementById('menuOverlay').addEventListener('click', function (e) {
   if (e.target === this) {
     toggleMenu();
   }
 });
 
 // Cerrar menú con tecla Escape
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
     const menuOverlay = document.getElementById('menuOverlay');
     if (menuOverlay.classList.contains('active')) {
