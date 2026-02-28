3e0adc27d79700d676c9842b56b090a633bcd9f4
AlvaroPersichini <alvaro.persichini@gmail.com>
2026-02-28 19:26:39 -0300

    update

--- script.js
+++ script.js
@@ -26,3 +26,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll(".animarTexto, .li-academic").forEach(el => observer.observe(el));
 });
+
+function iniciar() {
+  var elemento = document.getElementById("obtener");
+  elemento.addEventListener("click", obtenerUbicacion);
+}
+
+function obtenerUbicacion() {
+  var geoconfig = {
+    enableHighAccuracy: true,
+    timeout: 10000,
+    maximumAge: 60000
+  };
+  navigator.geolocation.getCurrentPosition(mostrar, mostrarError, geoconfig);
+}
+
+function mostrar(posicion) {
+  var ubicacion = document.getElementById("ubicacion");
+  var datos = "";
+  datos += "Latitud: " + posicion.coords.latitude + "<br>";
+  datos += "Longitud: " + posicion.coords.longitude + "<br>";
+  datos += "Exactitud: " + posicion.coords.accuracy + "mts.<br>";
+  ubicacion.innerHTML = datos;
+}
+
+function mostrarError(error) {
+
+  alert("Error: " + error.code + " " + error.message);
+}
+
+
+
+window.addEventListener("load", iniciar);
+
