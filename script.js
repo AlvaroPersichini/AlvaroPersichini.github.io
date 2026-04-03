4137b84950bea2971cadc4d512deadc570c2dda1
AlvaroPersichini <alvaro.persichini@gmail.com>
2026-04-03 10:01:03 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleMenu() {
   const menuOverlay = document.getElementById('menuOverlay');
   const btn = document.getElementById('hamburger');
-  document.body.classList.toggle('no-scroll');
   menuOverlay.classList.toggle('active');
   btn.classList.toggle('active');
 }
@@ -26,35 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll(".animarTexto, .li-academic").forEach(el => observer.observe(el));
 });
-
-function iniciar() {
-  var elemento = document.getElementById("obtener");
-  elemento.addEventListener("click", obtenerUbicacion);
-}
-
-function obtenerUbicacion() {
-  var geoconfig = {
-    enableHighAccuracy: true,
-    timeout: 10000,
-    maximumAge: 60000
-  };
-  navigator.geolocation.getCurrentPosition(mostrar, mostrarError, geoconfig);
-}
-
-function mostrar(posicion) {
-  var ubicacion = document.getElementById("ubicacion");
-  var datos = "";
-  datos += "Latitud: " + posicion.coords.latitude + "<br>";
-  datos += "Longitud: " + posicion.coords.longitude + "<br>";
-  datos += "Exactitud: " + posicion.coords.accuracy + "mts.<br>";
-  ubicacion.innerHTML = datos;
-}
-
-function mostrarError(error) {
-
-  alert("Error: " + error.code + " " + error.message);
-}
-
 
 
 window.addEventListener("load", iniciar);
